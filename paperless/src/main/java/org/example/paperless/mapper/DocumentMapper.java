9bded2a070dd1c172766e40e1ce7acfcb0bf6989
birunime <birunime@proton.me>
2026-09-18 20:43:55 +0200

    actual code

--- paperless/src/main/java/org/example/paperless/mapper/DocumentMapper.java
+++ paperless/src/main/java/org/example/paperless/mapper/DocumentMapper.java
@@ -1,7 +1,15 @@
 package org.example.paperless.mapper;
 
+import org.example.paperless.dto.DocumentPublic;
+import org.example.paperless.entity.Document;
+import org.mapstruct.Mapper;
+import org.mapstruct.MappingConstants;
 import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.Mapping;
 
-@Component
-public class DocumentMapper {
+@Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
+public interface DocumentMapper {
+
+
 }
+
